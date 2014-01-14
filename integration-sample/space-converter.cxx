d00e09b5cfb1e65aefa76246befdd7ed917f74d9
Usagi Ito <usagi@WonderRabbitProject.net>
2014-01-15 06:03:22 +0900

    space-converter実装

--- integration-sample/space-converter.cxx
+++ integration-sample/space-converter.cxx
@@ -1,1 +1,232 @@
 #include "space-converter.hxx"
+
+namespace arisin
+{
+  namespace etupirka
+  {
+    space_converter_t::space_converter_t
+    ( const a3d_t& top_camera_position__
+    , const a3d_t& front_camera_position__
+    , const float_t top_camera_angle_x__
+    , const float_t camera_fov_diagonal__
+    , const a2d_t& camera_sensor_size__
+    , const a2d_t& image_size__
+    )
+      : top_camera_position_(top_camera_position__)
+      , front_camera_position_(front_camera_position__)
+      , top_camera_angle_x_(top_camera_angle_x__)
+      , camera_fov_diagonal_(camera_fov_diagonal__)
+      , camera_sensor_size_(camera_sensor_size__)
+      , image_size_(image_size__)
+    {
+      L(INFO, "top_camera_positon[mm]: "    << to_string(top_camera_position__));
+      L(INFO, "front_camera_position[mm]: " << to_string(front_camera_position__));
+      L(INFO, "top_camera_angle_x[deg]: "    << top_camera_angle_x__);
+      L(INFO, "camera_fov_diagonal[deg]: "   << camera_fov_diagonal__);
+      L(INFO, "camera_sensor_size[mm]: "    << to_string(camera_sensor_size__));
+      L(INFO, "image_size[mm]: "            << to_string(image_size__));
+      
+      initialize();
+    }
+    
+    void space_converter_t::initialize()
+    {
+      // std::tanなどの三角関数はラジアン単位の仕様なので弧度法単位から変換しておく
+      // カメラのX軸回転角度[rad]
+      auto top_camera_angle_x_rad = degrees_to_radians(top_camera_angle_x_);
+      top_camera_angle_x_rad_ = top_camera_angle_x_rad;
+      L(INFO, "top_camera_angle_x_rad[rad]: " << top_camera_angle_x_rad);
+      
+      // カメラのセンサーの対角線の長さ
+      auto camera_sensor_diagonal = diagonal(x(camera_sensor_size_), y(camera_sensor_size_));
+      L(INFO, "camera_sensor_diagonal[mm]: " << camera_sensor_diagonal );
+      
+      // カメラの対角視野角[rad]
+      auto camera_fov_diagonal_rad = degrees_to_radians(camera_fov_diagonal_);
+      L(INFO, "camera_fov_diagonal_rad[rad]:" << camera_fov_diagonal_rad);
+      
+      // カメラの1/2対角視野角[rad]
+      auto camera_fov_diagonal_div_2_rad = camera_fov_diagonal_rad / 2.l;
+      L(INFO, "camera_fov_diagonal_div_2_rad[rad]: " << camera_fov_diagonal_div_2_rad);
+      
+      // カメラの焦点距離
+      auto camera_focal_length = camera_sensor_diagonal / std::tan(camera_fov_diagonal_div_2_rad) / 2.l;
+      L(INFO, "camera_focal_length[mm]: " << camera_focal_length);
+      
+      // カメラの1/2水平垂直視野角[rad]
+      a2d_t camera_fov_div_2_rad
+      {{ float_t( std::atan(x(camera_sensor_size_) / camera_focal_length / 2 ) )
+       , float_t( std::atan(y(camera_sensor_size_) / camera_focal_length / 2 ) )
+      }};
+      camera_fov_div_2_rad_ = camera_fov_div_2_rad;
+      L(INFO, "camera_fov_div_2_rad[rad]: " << to_string(camera_fov_div_2_rad));
+      
+#ifndef NDEBUG
+      // カメラの水平垂直視野角[rad](表示用)
+      a2d_t camera_fov_rad
+      {{ x(camera_fov_div_2_rad) * 2
+       , y(camera_fov_div_2_rad) * 2
+      }};
+      L(INFO, "camera_fov_rad[rad]: " << to_string(camera_fov_rad));
+      
+      // カメラの水平垂直視野角[deg](表示用)
+      a2d_t camera_fov
+      {{ radians_to_degrees(x(camera_fov_rad))
+      , radians_to_degrees(y(camera_fov_rad))
+      }};
+      L(INFO, "camera_fov[deg]: " << to_string(camera_fov));
+#endif
+      
+    }
+    
+    space_converter_t::a3d_t space_converter_t::operator()
+    ( const a2d_t& top_image_target
+    , const a2d_t& front_image_target
+    ) const
+    {
+      L(INFO, "top-image-target: "   << to_string(top_image_target));
+      L(INFO, "front-image-target: " << to_string(front_image_target));
+      
+      // top-camのターゲットのピクセル値をsnorm値に
+      //   ※snorm値: "signed normalized value"、日本語にすると符号付き正規化値
+      //            : 具体的には [-1.0〜+1.0] の範囲に、ものの値を正規化（尺度を整える）した値
+      //   ※unorm値: "unsigned normalized value"、日本語では符号無し正規化値
+      //            : 具体的には [ 0.0〜+1.0] に範囲に、ものの値を正規化した値
+      //            : 基本的にはsnormもunormも対象を最大値に対する比率で表す方法です。
+      a2d_t top_image_target_snorm
+      {{ uvalue_to_snorm(x(top_image_target), x(image_size_))
+       , uvalue_to_snorm(y(top_image_target), y(image_size_))
+      }};
+      L(INFO, "top_image_target_snorm[-]: " << to_string(top_image_target_snorm));
+      
+      // top-camのターゲットピクセルへの偏差角度
+      //   ※top-camの視線中心からターゲットピクセルへは(左右,上下)に何度ずれた射線となるか
+      //   ※要注意として、ここで、カメラのスクリーンに映ったイメージは
+      //     カメラがZ軸-を向いて撮影したものだから、
+      //     カメラスクリーンのX軸+方向は現実世界のX軸+方向と逆転しています。
+      //     なので、このカメラのスクリーン座標系から現実世界への座標系への変換に伴い、
+      //     X軸の符号反転が発生します。
+      a2d_t top_image_target_deviation_angle_rad
+      {{ -x(top_image_target_snorm) * x(camera_fov_div_2_rad_)
+       ,  y(top_image_target_snorm) * y(camera_fov_div_2_rad_)
+      }};
+      L(INFO, "top_image_target_deviation_angle_rad[rad]: " << to_string(top_image_target_deviation_angle_rad));
+      
+      // top-camとそのターゲットピクセルを通る直線の傾き角度
+      //   ※top-camはX軸回転(=「Z値に対する"Y値"の変動」)を持っている
+      a2d_t top_image_target_line_angle
+      {{ x(top_image_target_deviation_angle_rad)
+       , y(top_image_target_deviation_angle_rad) + top_camera_angle_x_rad_
+      }};
+      L(INFO, "top_image_target_line_angle[rad]: " << to_string(top_image_target_line_angle));
+      
+      // YZ平面(真横から見た平面図)における
+      // top-camとそのスクリーンの中心を通る直線の式 y = f(z)
+      // 
+      // YZ平面上の直線の方程式の傾きaと切片bによる標準形
+      // y = a * z + b;
+      //
+      // a = dy/dz = tan(θ)
+      //   θ = y(top_image_target_line_angle)
+      const auto top_yz_a = std::tan(y(top_image_target_line_angle));
+      // b = y - a * z
+      //  この直線の確実な通過点であるtop-camの位置座標(x,y,z)を代入
+      // b = y0 - a * z0
+      //  y0 = y(top_camera_position)
+      //  z0 = z(top_camera_position)
+      const auto top_yz_b = y(top_camera_position_) - top_yz_a * z(top_camera_position_);
+      L(INFO, "top YZ-plane line function [mm]: y = f(z) = " << top_yz_a << " z + " << top_yz_b);
+      
+      // front-camのターゲットのピクセル値をsnorm値に
+      a2d_t front_image_target_snorm
+      {{ uvalue_to_snorm(x(front_image_target), x(image_size_))
+       , uvalue_to_snorm(y(front_image_target), y(image_size_))
+      }};
+      L(INFO, "front_image_target_snorm [-]: " << to_string(front_image_target_snorm));
+      
+      // front-camのターゲットピクセルへの偏差角度
+      //   ※front-camの視線中心からターゲットピクセルへは(左右,上下)に何度ずれた射線となるか
+      a2d_t front_image_target_deviation_angle_rad
+      {{ x(front_image_target_snorm) * x(camera_fov_div_2_rad_)
+       , y(front_image_target_snorm) * y(camera_fov_div_2_rad_)
+      }};
+      L(INFO, "front_image_target [rad]: " << to_string(front_image_target_deviation_angle_rad));
+      
+      // top-camとそのターゲットピクセルを通る直線の傾き角度
+      //   ※front-camはXYZの全ての軸回転量は0で真正面を向いている
+      //     という前提なので偏差角度と等しい。
+      a2d_t front_image_target_line_angle
+      {{ x(front_image_target_deviation_angle_rad)
+       , y(front_image_target_deviation_angle_rad)
+      }};
+      L(INFO, "front_image_target_line_angle [rad]: " << to_string(top_image_target_line_angle));
+      
+      // YZ平面(真横から見た平面図)における
+      // front-camとそのスクリーンの中心を通る直線の式 y = f(z)
+      //   ※top-camと同様に傾き a と 切片 b を求める
+      const auto front_yz_a = std::tan(y(front_image_target_line_angle));
+      const auto front_yz_b = y(front_camera_position_) - front_yz_a * z(front_camera_position_);
+      L(INFO, "front YZ-plane line function [mm]: y = f(z) = " << front_yz_a << " z + " << front_yz_b);
+      
+      // YZ平面におけるtop-camのターゲット直線とfront-camのターゲット直線の交点を求める
+      //   2つの直線の式からなる連立方程式をyについて解く
+      //     y = top_yz_a   * z + top_yz_b    ... (1)
+      //     y = front_yz_a * z + front_yz_b  ... (2)
+      //  代入: (1):y <-- (2):y
+      //     top_yz_a * z + top_yz_b = front_yz_a * z + front_yz_b
+      //     top_yz_a * z - front_yz_a * z = front_yz_b - top_yz_b
+      //     z (top_yz_a - front_yz_a) = front_yz_b - top_yz_b
+      //     z = (front_yz_b - top_yz_b) / (top_yz_a - front_yz_a)  ... (3)
+      //  代入: (1):z <-- (3):z
+      //     y = top_yz_a * {(front_yz_b - top_yz_b) / (top_yz_a - front_yz_a)} + top_yz_b
+      const auto cross_point_y = top_yz_a * ( (front_yz_b - top_yz_b) / (top_yz_a - front_yz_a) ) + top_yz_b;
+      L(INFO, "cross point y [mm]: " << cross_point_y);
+      
+      // top-cam について x = f(y) と
+      //                  z = f(y) の関数式を定義する
+      // z = f(y)
+      auto z_from_y = [&](const float_t y_value)
+      {
+        // y = a * z + b を z について変形して
+        // z = (y - b) / a となるので、
+        return (y_value - top_yz_b) / top_yz_a;
+      };
+      L(INFO, "z_from_y prepared");
+      
+      // x = f(y)
+      // XZ平面についてはまだ考えていないので、
+      // YZ平面の時と同様にして新たに算出する。
+      auto x_from_z = [&](const float_t z_value)
+      {
+        // 一次方程式の傾きaと切片bの標準形より
+        // x = a * z + b
+        //   a = dx/dy = tan(φ) ※φ（読み: ふぁい、θはさっきもう使ったから次の文字、というだけ）
+        //   φ = x(top_image_target_line_angle)
+        const auto top_xz_a = std::tan(x(top_image_target_line_angle));
+        //   b = x - a * z
+        //     top-cam の位置座標(x,y,z)より x と z を代入して
+        //   b = x(top_camera_position) - top_xy_a * z(top_camera_position)
+        const auto top_xz_b = x(top_camera_position_) - top_xz_a * z(top_camera_position_);
+        // 以上、 x = f(z) における傾きaと切片bを求めたので x について解ける
+        L(INFO, "top XZ-plane line function   [mm]: x = f(y) = " << top_xz_a << " * z + " << top_xz_b);
+        return top_xz_a * z_value + top_xz_b;
+      };
+      L(INFO, "x_from_z prepared");
+      
+      // 実空間の座標値(x,y,z)における y が定まり、
+      // x = f(y)
+      // z = f(y)
+      // が与えられているので、
+      // top-camとfront-camのターゲットピクセルの示す現実空間の完全な座標値が明らかとなる。
+      const a3d_t cross_point
+      {{ x_from_z(z_from_y(cross_point_y))
+      , cross_point_y
+      , z_from_y(cross_point_y)
+      }};
+      L(INFO, "cross_point [mm]: " << to_string(cross_point));
+      
+      return std::move(cross_point);
+    }
+    
+  }
+}
--- integration-sample/space-converter.hxx
+++ integration-sample/space-converter.hxx
@@ -1,11 +1,79 @@
 #pragma once
 
+#include <cmath>
+#include <array>
+#include <iostream>
+#include <memory>
+#include <typeinfo>
+#include <boost/math/constants/constants.hpp>
+#include "logger.hxx"
+
 namespace arisin
 {
   namespace etupirka
   {
-    class space_converter_t
+    class space_converter_t final
     {
+    public:
+      using float_t = float;
+      using a3d_t = std::array<float_t, 3>;
+      using a2d_t = std::array<float_t, 2>;
+    
+    private:
+      template<class T> constexpr typename T::value_type x(const T& a) { return a[0]; }
+      template<class T> constexpr typename T::value_type y(const T& a) { return a[1]; }
+      template<class T> constexpr typename T::value_type z(const T& a) { return a[2]; }
+      
+      template<class T_in_out, class T_internal = float_t>
+      constexpr T_in_out degrees_to_radians(T_in_out degrees)
+      { return T_internal(degrees) * boost::math::constants::pi<T_internal>() / T_internal(180); }
+      
+      template<class T_in_out, class T_internal = float_t>
+      constexpr T_in_out radians_to_degrees(T_in_out radians)
+      { return T_internal(radians) * T_internal(180) / boost::math::constants::pi<T_internal>(); }
+
+      template<class T> constexpr T pow2(const T v){ return v * v; }
+      template<class T> constexpr T diagonal(const T a, const T b){ return std::sqrt( pow2(a) + pow2(b) ); }
+
+      template<class T> constexpr T uvalue_to_snorm(const T v, const T max)
+      { return (v - (max / 2)) / (max / 2); }
+      
+      template<class T>
+      std::string to_string(const T& vs) const
+      {
+        std::string r;
+        for(auto v: vs)
+        {
+          r += v;
+          r += ',';
+        }
+        r.resize(r.size() - 1);
+        return r;
+      }
+
+      const a3d_t top_camera_position_;
+      const a3d_t front_camera_position_;
+      const float_t top_camera_angle_x_;
+      const float_t camera_fov_diagonal_;
+      const a2d_t camera_sensor_size_;
+      const a2d_t image_size_;
+      
+      a2d_t camera_fov_div_2_rad_;
+      float_t top_camera_angle_x_rad_;
+      
+    public:
+      space_converter_t
+      ( const a3d_t& top_camera_position__
+      , const a3d_t& front_camera_position__
+      , const float_t top_camera_angle_x_deg__
+      , const float_t camera_fov_diagonal_deg__
+      , const a2d_t& camera_sensor_size__
+      , const a2d_t& image_size__
+      );
+      
+      void initialize();
+      
+      a3d_t operator()(const a2d_t& top_image_target, const a2d_t& front_image_target) const;
     };
   }
 }
