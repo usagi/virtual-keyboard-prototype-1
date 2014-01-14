03558c5082cdaa77faeb7682713bdfa44ad0e456
Usagi Ito <usagi@WonderRabbitProject.net>
2014-01-14 16:12:58 +0900

    sender実装

--- integration-sample/udp-sender.cxx
+++ integration-sample/udp-sender.cxx
@@ -1,1 +1,34 @@
 #include "udp-sender.hxx"
+
+namespace arisin
+{
+  namespace etupirka
+  {
+    udp_sender_t::udp_sender_t(const std::string& address__, const int port__)
+      : resolver(io_service)
+      , query(boost::asio::ip::udp::v4(), address__, std::to_string(port__).data())
+      , endpoint(*resolver.resolve(query))
+      , socket(io_service)
+      , address_(address__)
+      , port_(port__)
+    {
+      L(INFO, "resolver, query, endpoint, socket are initialized");
+      L(INFO, "address(" << address__ << ") port(" << port__ << ")" );
+      socket.open(boost::asio::ip::udp::v4());
+      L(INFO, "socket opened");
+    }
+    
+    void udp_sender_t::operator()(const std::string& message) const
+    {
+      L(INFO, "message: " << message);
+      //socket.send_to(boost::asio::buffer(message), endpoint);
+      L(INFO, "message sent");
+    }
+    
+    const std::string& udp_sender_t::address() const
+    { return address_; }
+    
+    const int udp_sender_t::port() const
+    { return port_; }
+  }
+}
--- integration-sample/udp-sender.hxx
+++ integration-sample/udp-sender.hxx
@@ -1,11 +1,35 @@
 #pragma once
 
+#include <string>
+
+#include <boost/asio.hpp>
+#include <boost/program_options.hpp>
+
+#include "logger.hxx"
+
 namespace arisin
 {
   namespace etupirka
   {
     class udp_sender_t final
     {
+      void initialize();
+      
+      boost::asio::io_service io_service;
+      
+      boost::asio::ip::udp::resolver        resolver;
+      boost::asio::ip::udp::resolver::query query;
+      boost::asio::ip::udp::endpoint        endpoint;
+      boost::asio::ip::udp::socket          socket;
+      
+      std::string address_;
+      int port_;
+      
+    public:
+      udp_sender_t(const std::string& address__ = "localhost", const int port = 30000);
+      void operator()(const std::string& message) const;
+      const std::string& address() const;
+      const int port() const;
     };
   }
 }
