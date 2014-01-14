968fd54cede8645bddb9d9041015736313ab9cf3
Usagi Ito <usagi@WonderRabbitProject.net>
2014-01-14 17:52:49 +0900

    udp-reciever実装

--- integration-sample/udp-reciever.cxx
+++ integration-sample/udp-reciever.cxx
@@ -1,1 +1,38 @@
 #include "udp-reciever.hxx"
+
+namespace arisin
+{
+  namespace etupirka
+  {
+    udp_reciever_t::udp_reciever_t(const int port__)
+      : socket(io_service, boost::asio::ip::udp::endpoint(boost::asio::ip::udp::v4(), port__))
+      , port_(port__)
+    {
+      L(INFO, "socket is initialized");
+      L(INFO, "port(" << port__ << ")" );
+    }
+    
+    std::string udp_reciever_t::operator()()
+    {
+      using boost::asio::ip::udp;
+      
+      boost::array<char, 4096>  buffer;
+      udp::endpoint             endpoint;
+      boost::system::error_code error;
+      
+      auto len = socket.receive_from(boost::asio::buffer(buffer), endpoint, 0, error);
+      
+      L(INFO, "result of socket.recieve_from: len(" << len << ") endpoint(" << endpoint.address().to_string() << ") error(" << error << ")");
+      
+      if(error && error != boost::asio::error::message_size)
+        throw boost::system::system_error(error);
+      
+      L(INFO, "recieve message" << std::string(buffer.data(), buffer.data()));
+      
+      return { reinterpret_cast<const char*>(buffer.data()), size_t(len) };
+    }
+    
+    const int udp_reciever_t::port() const
+    { return port_; }
+  }
+}
--- integration-sample/udp-reciever.hxx
+++ integration-sample/udp-reciever.hxx
@@ -1,11 +1,25 @@
 #pragma once
 
+#include <string>
+
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
+
+#include "logger.hxx"
+
 namespace arisin
 {
   namespace etupirka
   {
     class udp_reciever_t final
     {
+      boost::asio::io_service      io_service;
+      boost::asio::ip::udp::socket socket;
+      int port_;
+    public:
+      udp_reciever_t(const int port__);
+      std::string operator()();
+      const int port() const;
     };
   }
 }
