5a13902ede7ee576ce54dbdf6ec7f6d47e516c2b
Usagi Ito <usagi@WonderRabbitProject.net>
2014-01-29 03:52:46 +0900

    #72 OSXでvirtual-keyboard.build.shがsedに-rなんて知らんとか言う問題の修正

--- integration-sample/virtual-keyboard.build.sh
+++ integration-sample/virtual-keyboard.build.sh
@@ -5,13 +5,24 @@ echo "    * source-dir: $1"
 echo "    * build-dir : $2"
 echo "    * target-dir: $3"
 
+if [ `uname -s` = 'Darwin' ]; then
+  echo "    * OS        : Darwin"
+  sed_option_ex='-E';
+else
+  echo "    * OS        : GNU/Linux or other"
+  sed_option_ex='-r';
+fi
+
+echo "    * sed option: ${sed_option_ex}"
+
 if test -f virtual-keyboard.sqlite3
 then
   rm -v virtual-keyboard.sqlite3
 fi
 
 echo "    generate sqlite3 importable data: $2/virtual-keyboard-data.csv"
-tail -n +2 $1/virtual-keyboard.csv | sed -r "s/^(([^,]+,){5}[^,]+),.*/\\1/g" > $2/virtual-keyboard-data.csv
+
+tail -n +2 $1/virtual-keyboard.csv | sed $sed_option_ex "s/^(([^,]+,){5}[^,]+),.*/\\1/g" > $2/virtual-keyboard-data.csv
 
 echo "    create table: $3/virtual-keyboard.sqlite3"
 sqlite3 $3/virtual-keyboard.sqlite3 "create table test(x real,y real,w real,h real,s real,id integer)" || exit 1
